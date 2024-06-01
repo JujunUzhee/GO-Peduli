07a47c8b3ccd83426e455c0b32db0be46201bdb4
DickyGinting <dickyginting67@gmail.com>
2024-06-01 17:12:10 +0700

    Menambahkan fungsi untuk formulir mitra, dan menambahkan http route post /formulir-mitra

--- Backend/index.js
+++ Backend/index.js
@@ -15,8 +15,37 @@ app.get("/", (req, res) => {
   return res.status(200).json("ok")
 });
 
+/**
+ * Fungsi tambah mitra
+ * @param {*} nama 
+ * @param {*} nowa 
+ * @param {*} email 
+ * @param {*} lokasi 
+ * @param {*} kategori_pakaian 
+ * @param {*} pengantar 
+ * @param {*} alamat 
+ */
+const daftarMitra = async (nama, nowa, email, lokasi, kategori_pakaian, pengantar, alamat) => {
+  let sql = "INSERT INTO formulir_mitra(nama, nowa, email, lokasi, kategori_pakaian, pengantar, alamat) VALUES (?, ?, ?, ?, ?, ?, ?)"
+  await query(sql, [nama, nowa, email, lokasi, kategori_pakaian, pengantar, alamat]);
+}
+
+app.post("/formulir-mitra", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const data = req.body
+  console.log(data)
+  await daftarMitra(data.nama, data.nowa, data.email, data.lokasi, data.kategori_pakaian, data.pengantar,
+    data.alamat
+  )
+
+  // return status is created
+  return res.status(201).json({
+    "status": "success",
+    "message": "Berhasil mendaftarkan mitra dengan nama alamat email: " + data.email,
+  })
+})
+
 app.listen(process.env.PORT_DEV, () => {
     testConnection()
   console.log(`Server started on http://localhost:${process.env.PORT_DEV}`);
 });
- 
